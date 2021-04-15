92096b13ba2b30295592905585ede15cd57e5e04
HildaMendoza-Avila <43427821+HildaMendoza-Avila@users.noreply.github.com>
2021-04-15 12:50:46 -0500

    Initial Data Files and Scripts

---
+++ DataExperimentationScript.R
@@ -0,0 +1,3 @@
+# install.packages("tigris")
+# install.packages("mapview")
+# link to the assignment specifications: https://www.evl.uic.edu/aej/424/21Sproject3.html
---
+++ dirSetup.R
@@ -0,0 +1,2 @@
+setwd('..')
+setwd('Desktop/CS 424/CS424-Project 03/')
